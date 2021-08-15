39f90ce133b8bc142a0ab98d5f7921149e20e101
dddong1234 <sdh080200@gmail.com>
2021-08-16 00:19:11 +0900

    added Ex13_1 to Ex13_10

---
+++ src/com/lilspicy/ch13/Ex13_2.java
@@ -0,0 +1,19 @@
+package com.lilspicy.ch13;
+
+public class Ex13_2 {
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < 1000000; i++) {
+            System.out.printf("%s", new String("-"));
+        }
+        System.out.println();
+        System.out.print("소요시간1: " + (System.currentTimeMillis() - startTime));
+
+        for (int i = 0; i < 1000000; i++) {
+            System.out.printf("%s", new String("|"));
+        }
+        System.out.println();
+        System.out.print("소요시간2: " + (System.currentTimeMillis() - startTime));
+    }
+}
